231aa6e5a9b0d7c4f72518d8c778c3a4a98f85f2
Zack Roth <zack27r@gmail.com>
2022-10-25 01:33:48 -0400

    Committing for desktop

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,26 @@
-const About = () => {};
+const About = () => {
+  return (
+    <div className="p-6 fade-in-page">
+      <h1 className="text-5xl m-3">
+        <a href="/">
+          <b>zachary roth </b>
+        </a>
+        / about
+      </h1>
+      <h3 className="text-2xl m-3">
+        hi! i&#39;m zachary. you can call me zack, though.
+      </h3>
+      <p className="text-lg m-3">
+        i&#39;m currently a student at northeastern university studying computer
+        science, with a concentration in software development. if all goes to
+        plan, i should be graduating at the end of 2023.
+      </p>
+      <p className="text-lg m-3">
+        when i&#39;m not tinkering with code, i have about a million different
+        hobbies.
+      </p>
+    </div>
+  );
+};
 
 export default About;
